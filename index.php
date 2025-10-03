7d3152b875f131ec177f4adea71a832e9afd1cdf
westras <r.westholm@telia.com>
2025-10-03 10:17:15 +0200

    Done

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Första Försöket</title>
+</head>
+<body>
+<div>
+<?php
+for ($i = 1; $i < 11; $i++) {
+    echo "Ett får två får tre får fyra får fem får sex får sju får 6️⃣🤷‍♂️7️⃣ ".$i."<br>";
+}
+
+?>
+</div>
+</body>
+</html>
